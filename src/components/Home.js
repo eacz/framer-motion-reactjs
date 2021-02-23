f1f2a3a075ae274d46d9645fefe494ad893f526a
eacz <estebanacanteros@gmail.com>
2021-02-23 13:23:25 -0300

    keyframes

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,17 +3,23 @@ import { Link } from 'react-router-dom';
 import { motion } from 'framer-motion';
 import { glowButton } from '../sharedAnimations';
 
+const buttonVariants = {
+    hover: {
+        ...glowButton,
+    },
+};
+
 const Home = () => {
     return (
         <motion.div
             initial={{ opacity: 0 }}
             animate={{ opacity: 1 }}
             transition={{ delay: 0.3, duration: 1.5 }}
-            className='home container'>
+            className='home container'
+        >
             <motion.h2>Welcome to Pizza Joint</motion.h2>
             <Link to='/base'>
-                <motion.button
-                    whileHover={glowButton}>
+                <motion.button variants={buttonVariants} animate='visible' whileHover='hover'>
                     Create Your Pizza
                 </motion.button>
             </Link>
--- src/sharedAnimations.js
+++ src/sharedAnimations.js
@@ -3,8 +3,16 @@ export const glowButton = {
     scale: 1.1,
     textShadow: '0px 0px 8px rgb(255,255,255)',
     boxShadow: '0px 0px 8px rgb(255,255,255)',
+    transition: {
+        duration: 0.3,
+        yoyo: Infinity, //infinite keyframes that switch between the original state and the properties specified upper
+    },
 };
 
-export const liWhileHover = { scale: 1.3, originX: 0, color: '#f8e112' }
-export const liTransition = { type: 'spring', stiffness: 300 }
+export const liWhileHover = {
+    originX: 0,
+    color: '#f8e112',
+    scale: 1.1,
 
+};
+export const liTransition = { type: 'spring', stiffness: 300 };
